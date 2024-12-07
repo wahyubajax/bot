fdfc7586f46f3e9e1825f0a706280a57bd641180
wahyubajax <yuyuu358@gmail.com>
2024-12-08 04:40:40 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,29 @@
+import os
+import time
 
+from program import Client
+from pyrogram.enums import ParseMode
+from pyrogram.types import InlineKeyboardMarkup, InlineQueryResult
+
+StartTime = time.time()
+
+# User bot
+Yuu = Client(
+  name="Yuu",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  session_string=SESSION,
+  parse_mode=ParseMode.HTML,
+  plugins=dict(root="bot"),
+)
+
+# Bot nya
+Bot = Client(
+  name="Bot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN,
+  in_memory=False,
+  parse_mode=ParseMode.HTML,
+  plugins=dict(root="bot"),
+)
