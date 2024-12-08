682991be26b9f4c5b31e6fe9328f5397f7f04067
wahyubajax <yuyuu358@gmail.com>
2024-12-08 14:34:17 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,7 +5,8 @@ async def main():
     await Bot.start()
     await Yuu.start()
     await pyrogram.idle()
-
+    await asyncio.gather(*tasks, Yuu.start(), Bot.start())
 
 if __name__ == "__main__":
-    Yuu.loop.run_until_complete(main())
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    loop.run_until_complete(main())
