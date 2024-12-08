b3979f96b48479f0af450a3c9905e8c4f535a133
wahyubajax <yuyuu358@gmail.com>
2024-12-08 15:58:59 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,18 +3,16 @@ import config
 import asyncio
 from importlib import import_module
 
-from pyrogram import idle
 from bot import Yuu , Bot
 from pyrogram import Client
 
-async def run_client():
+async def main():
     await Bot.start()
     await Yuu.start()
-    await pyrogram.idle()
     await asyncio.gather(*tasks, Yuu.start(), Bot.start())
-    await asyncio.gather(loadPlugins(), idle())
+    await asyncio.gather(loadPlugins()
 
 print("start")
 
 if __name__ == "__main__":
-    Yuu.loop.run_until_complete(run_clients())
+    Yuu.loop.run_until_complete(main())
