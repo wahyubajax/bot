383335c99403e3262c7c50459391eec4412ae612
wahyubajax <yuyuu358@gmail.com>
2024-12-08 17:17:24 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,6 +4,7 @@ import logging
 
 from pyrogram import Client
 from pyrogram.enums import ParseMode
+from pyrogram.handlers import MessageHandler
 from pyrogram.types import InlineKeyboardMarkup, InlineQueryResult
 from config import API_ID, API_HASH, BOT_TOKEN, SESSION
 
@@ -14,6 +15,26 @@ FORMAT = "[Yuu]: %(message)s"
 logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
                                                     logging.StreamHandler()], format=FORMAT)
 MODULE = []
+
+class ConnectionHandler(logging.Handler):
+    def emit(self, record):
+        for X in ["OSErro", "socket"]:
+            if X in record.getMessage():
+                os.system(f"kill -9 {os.getpid()} && python3 -m PyroUbot")
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s", "%d-%b %H:%M")
+stream_handler = logging.StreamHandler()
+
+stream_handler.setFormatter(formatter)
+connection_handler = ConnectionHandler()
+
+logger.addHandler(stream_handler)
+logger.addHandler(connection_handler)
+
 
 class Yuu(Client):
     def __init__(self, **kwargs):
