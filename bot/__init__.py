6d48da6f8627d531a20e8364b9c9eb7434089d2e
wahyubajax <yuyuu358@gmail.com>
2024-12-08 17:30:40 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -16,6 +16,27 @@ logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'
                                                     logging.StreamHandler()], format=FORMAT)
 MODULE = []
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
+
 class ConnectionHandler(logging.Handler):
     def emit(self, record):
         for X in ["OSErro", "socket"]:
@@ -73,6 +94,7 @@ Yuu = Client(
   api_hash=API_HASH,
   session_string=SESSION,
   parse_mode=ParseMode.HTML,
+  plugins={"root": "bot/modules"},
 )
 
 # Bot nya
@@ -83,4 +105,5 @@ Bot = Client(
   bot_token=BOT_TOKEN,
   in_memory=False,
   parse_mode=ParseMode.HTML,
+  plugins={"root": "bot/modules"},
 )
