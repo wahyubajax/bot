702857f92d20d0601ec095e88205b385fb33a2b0
wahyubajax <yuyuu358@gmail.com>
2024-12-08 15:23:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7743669182:AAFkT8Slh5upv-lOK9irf7X0zy_i
 OWNER_ID = int(os.getenv("OWNER_ID", "6111508566"))
 COMMAND = os.getenv("COMMAND", ". , : ; !")
 PREFIX = COMMAND.split()
-SESSION_STRING = os.getenv(
-    "SESSION_STRING",
+SESSION = os.getenv(
+    "SESSION",
     "BQGvJ4IAGeB4x-if3HkJElE4_OD_t1wACzi23u3hoI19sqeePW-55CzXtY1ixB03PB0FJQilTjqcRMvUloz0tjKnffy1vKTlrWwnZ4bXWJk1cUZy6noW19p7SLHZCRq6-4xpNlHAWpCgecBQieXa-dCvZ5En4mcMxwMiJ3_HuKWU29nfT9reTsRbUDk8XtPiZUBIzJE548iI14SJgm9prs6c5Cxs6QhXToBvpWkVcxw0xrOSDcX-9O-RFIANy8x5ACv1B5he77XhrhQi42TGqEsEcBNtqp3fThOrJDXSikazHkPNuoYOcJXSsBnKyxZp1kDUDA16p0PE4B4lwwmfGcsyuFFXrQAAAAFsRjhWAA"
 )
