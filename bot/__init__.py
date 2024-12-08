2a53e3da44d5658bf686f255777aaba71024eecd
wahyubajax <yuyuu358@gmail.com>
2024-12-08 14:31:01 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,46 @@
 import os
 import time
+import logging
 
 from program import Client
 from pyrogram.enums import ParseMode
 from pyrogram.types import InlineKeyboardMarkup, InlineQueryResult
 
 StartTime = time.time()
+
+FORMAT = "[Yuu]: %(message)s"
+
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+                                                    logging.StreamHandler()], format=FORMAT)
+class Yuu(Client):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, parse_mode=ParseMode.HTML)
+
+    def on_message(self, filters=None, group=-1):
+        def decorator(func):
+            self.add_handler(MessageHandler(func, filters), group)
+            return func
+
+        return decorator
+
+    async def start(self):
+        await super().start()
+
+
+class Bot(Client):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, parse_mode=ParseMode.HTML)
+
+    def on_message(self, filters=None, group=-1):
+        def decorator(func):
+            for ub in self._ubot:
+                ub.add_handler(MessageHandler(func, filters), group)
+            return func
+
+        return decorator
+
+    async def start(self):
+        await super().start()
 
 # User bot
 Yuu = Client(
