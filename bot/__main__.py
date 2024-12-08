8baeae9d87ad4f1bcd2e3027e56a0defe99c9588
wahyubajax <yuyuu358@gmail.com>
2024-12-08 14:13:16 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,1 +1,11 @@
+import os
+import config
 
+async def main():
+    await Bot.start()
+    await Yuu.start()
+    await pyrogram.idle()
+
+
+if __name__ == "__main__":
+    Yuu.loop.run_until_complete(main())
