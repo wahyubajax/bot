6dfa8a09392da24bda0f927ce6a31351b4fab1ee
wahyubajax <yuyuu358@gmail.com>
2024-12-08 16:00:31 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,7 +11,6 @@ async def main():
     await Yuu.start()
     await asyncio.gather(*tasks, Yuu.start(), Bot.start())
     await asyncio.gather(loadPlugins()
-
 print("start")
 
 if __name__ == "__main__":
