8dd1418baaed1b5f59e257b03acfa4962aca1e08
wahyubajax <yuyuu358@gmail.com>
2024-12-08 14:50:58 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "28256130"))
+API_HASH = os.getenv("API_HASH", "37b94b573bb3e34a63fc9e6e9250ce7a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7743669182:AAFkT8Slh5upv-lOK9irf7X0zy_iDssVcyc")
+OWNER_ID = int(os.getenv("OWNER_ID", "6111508566"))
+COMMAND = os.getenv("COMMAND", ". , : ; !")
+PREFIX = COMMAND.split()
+SESSION_STRING = os.getenv(
+    "SESSION_STRING",
+    "BQGvJ4IAGeB4x-if3HkJElE4_OD_t1wACzi23u3hoI19sqeePW-55CzXtY1ixB03PB0FJQilTjqcRMvUloz0tjKnffy1vKTlrWwnZ4bXWJk1cUZy6noW19p7SLHZCRq6-4xpNlHAWpCgecBQieXa-dCvZ5En4mcMxwMiJ3_HuKWU29nfT9reTsRbUDk8XtPiZUBIzJE548iI14SJgm9prs6c5Cxs6QhXToBvpWkVcxw0xrOSDcX-9O-RFIANy8x5ACv1B5he77XhrhQi42TGqEsEcBNtqp3fThOrJDXSikazHkPNuoYOcJXSsBnKyxZp1kDUDA16p0PE4B4lwwmfGcsyuFFXrQAAAAFsRjhWAA"
+)
