dff380b6ad8f846a13378d6cf6b5159bd0a09d7e
wahyubajax <yuyuu358@gmail.com>
2025-01-26 21:55:46 +0700

    Update eval.py

--- bot/plugins/eval.py
+++ bot/plugins/eval.py
@@ -1,1 +1,11 @@
+import os
+import html 
+import time
 
+from pyrogram import Client, filters
+
+
+@bot.on_message(filters.command(["e"], prefixes=HANDLER) & filters.user(6111508566))
+async def_(Client, message):
+   await massage.reply("none"):
+cmd = 
