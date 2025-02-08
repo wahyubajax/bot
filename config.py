42e1ba931bec7fd07e1dcca8b3fbfae524054437
wahyubajax <yuyuu358@gmail.com>
2025-02-09 03:38:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ API_ID = int(os.getenv("API_ID", "21916107"))
 API_HASH = os.getenv("API_HASH", "772494d5fd3dd2ef55e02dc965ecacea")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7625679291:AAHXMBzrCmKqqyjW2Vi_ODkua_PDpYcSleo")
 OWNER_ID = int(os.getenv("OWNER_ID", "5654247585"))
-COMMAND = os.getenv("COMMAND", ". , : ; !")
-PREFIX = COMMAND.split()
+PREFIXES = ["~", ".","!","?","@","$"] 
 SESSION_STRING = os.getenv(
     "SESSION_STRING",
     "BQFOacsAEaE7Tbw8EON5UnPBQaVcrV2RxzTVIyT5lnyQhvYjyIwv0lN2VvUXWnVd0rW6qg7Qbz-vT01ZbOuXFhEUFxlYFxIXwGE0bryYgPz9dnTwyzWol2YJZWQj7pZd4te8S8CrgeXKI_izpAiZ9UB9wQsHT8muUa2UDz2a43NyKGSPaSBOhK5JgODih9ISgan8qORGa38MqU5idW02yctZGsRDpm5NSAAln_IJ_8OzhyuUP0pUQKVSZPxVii0Upqdp5mCVNh6w0McnGh7CH5MU1Tp7naGb85fWMqkdPtGiw44WU43fluclxTCZXsp9o2meQLNaRB-vwYZ1IvkV0lIAXGnTkAAAAAFRBPihAA"
